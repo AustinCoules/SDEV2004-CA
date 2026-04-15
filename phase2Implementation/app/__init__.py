5edeca47ccee1b23c60c3a1db08054553d96cbfc
Austin Coules <c23444946@mytudublin.ie>
2026-04-15 11:51:41 +0100

    Reuse __init__ code from labs to start

--- phase2Implementation/app/__init__.py
+++ phase2Implementation/app/__init__.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, session
+from flask_babel import Babel
+
+def get_locale():
+    if 'language' in session:
+        return session['language']
+    return request.accept_languages.best_match(['en', 'de', 'pl'])
+
 app = Flask(__name__)
+
+app.secret_key='asecretkey' # TODO: change this to a secure key in production
+
+babel = Babel(app)
+
+babel.init_app(app, locale_selector=get_locale)
+
 from app import routes
