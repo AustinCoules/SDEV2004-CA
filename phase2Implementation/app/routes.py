a5ed9c9c9746b403768cc69dd44df8ca9a6733dc
Austin Coules <c23444946@mytudublin.ie>
2026-04-17 13:05:09 +0100

    Fix redundant brackets

--- phase2Implementation/app/routes.py
+++ phase2Implementation/app/routes.py
@@ -5,7 +5,7 @@ from flask import render_template, session, redirect, request
 @app.route('/')
 @app.route('/index')
 def index():
-    return (render_template("index.html"))
+    return render_template("index.html")
 
 # Austin
 @app.route('/checkout')
